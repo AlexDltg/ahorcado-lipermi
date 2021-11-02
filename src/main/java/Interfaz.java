c702df65775bcb6d72a3c4bc63dbb3faa14d8d55
JesusSePe <umbreon20019@gmail.com>
2021-11-02 19:03:18 +0100

    server almost done

--- src/main/java/Interfaz.java
+++ src/main/java/Interfaz.java
@@ -5,7 +5,7 @@ import java.rmi.RemoteException;
 	Declarar firma de métodos que serán sobrescritos
 */
 public interface Interfaz extends Remote {
-    String guess(String word) throws RemoteException;
+    String guess(char letter) throws RemoteException;
     String init() throws RemoteException;
-    Integer attempt() throws RemoteException;
+    int fallos() throws RemoteException;
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,30 +1,90 @@
+import net.sf.lipermi.exception.LipeRMIException;
 import net.sf.lipermi.handler.CallHandler;
+import net.sf.lipermi.net.IServerListener;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Server {
-    CallHandler callHandler = new CallHandler();
-    Interfaz ImplementacionDeInterfaz = new Interfaz() {
-        @Override
-        public String guess(String word) throws RemoteException {
-
-            return null;
-        }
-
-        @Override
-        public String init() throws RemoteException {
-            return null;
-        }
-
-        @Override
-        public Integer attempt() throws RemoteException {
-            return null;
-        }
-    };
-    callHandler.registerGlobal(Interfaz.class, ImplementacionDeInterfaz);
-
-    Server server = new Server();
     int puerto = 4455;
-    server.bind(puerto, callHandler);
+    CallHandler callHandler = new CallHandler();
+    net.sf.lipermi.net.Server server = new net.sf.lipermi.net.Server();
+    String[] words = new String[] {"PREGUNTA", "SERVIDOR", "CLIENTE", "PROGRAMACION", "ORDENADOR"};
 
+    public Server() throws LipeRMIException, IOException {
+        Remote ImplementacionDeInterfaz = UnicastRemoteObject.exportObject(new Interfaz() {
+            int fallos = 0;
+            String word = "";
+            final ArrayList<Character> status = new ArrayList<>();
+
+            @Override
+            public String guess(char letter) throws RemoteException {
+                boolean found = false;
+                for (int i = 0; i < word.length(); i++) {
+                    if( String.valueOf(letter).toUpperCase(Locale.ROOT).equals(String.valueOf(word.charAt(i)))) {
+                        found = true;
+                        status.set(i, letter);
+                    }
+                }
+                if (!found) {
+                    fallos++;
+                }
+                if (status.stream().map(Objects::toString).collect(Collectors.joining("")).equalsIgnoreCase(word)){
+                    fallos = -1;
+                }
+                System.out.println("Status: " + status.stream().map(Objects::toString).collect(Collectors.joining(" ")));
+                System.out.println("Word: " + word);
+                return status.stream().map(Objects::toString).collect(Collectors.joining(" "));
+            }
+
+            @Override
+            public String init() throws RemoteException {
+                word = words[(int)(Math.random() * (4)+1)];
+                for (int i = 0; i < word.length(); i++) {
+                    status.add('_');
+                }
+                System.out.println("Word: " + word);
+                return status.stream().map(Objects::toString).collect(Collectors.joining(" "));
+            }
+
+            @Override
+            public int fallos() throws RemoteException {
+                System.out.println("Fallos: " + fallos);
+                return fallos;
+            }
+        }, 0);
+
+        callHandler.registerGlobal(Interfaz.class, ImplementacionDeInterfaz);
+
+        server.bind(puerto, callHandler);
+
+        server.addServerListener(new IServerListener() {
+            @Override
+            public void clientConnected(Socket socket) {
+                System.out.println("Usuario Conectado desde: " + socket.getInetAddress());
+            }
+
+            @Override
+            public void clientDisconnected(Socket socket) {
+                System.out.println("Usuario Desconectado");
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public static void main (String[] args) throws LipeRMIException, IOException {
+        new Server();
+        System.out.println("Server started");
+    }
 }
